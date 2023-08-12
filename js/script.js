e4034a07ca354aaf1d84dae3dcfc8d3521e5957d
gianluca cattin <gcattin97@gmail.com>
2023-08-12 23:56:49 +0200

    a lot of new tings

--- js/script.js
+++ js/script.js
@@ -1,33 +1,75 @@
 window.onload = function() {
   const banner = document.querySelector('.banner');
-  const header = document.querySelector('.header');
-  const closeButton = document.querySelector('#closeBtn')
+  const popup = document.querySelector('.banner-pop');
+  const headerBanner = document.querySelector('.header');
+  const headerPopup = document.querySelector('.header-pop');
+  const closeBannerButton = document.querySelector('#closeBannerBtn');
+  const closePopupButton = document.querySelector('#closePopupBtn');
+  const showPopUp = document.querySelector('#projects-folder')
+  const popupContainer = document.querySelector('#popupContainer')
 
   let mouseDown = false;
   let startX, startY, initialLeft, initialTop;
+  let currentDragFunction = null;
+  let isPopupVisible = false;
 
-  const calculatePosition = (e) => {
+  const calculatePosition = (elem) => (e) => {
     const dx = e.clientX - startX;
     const dy = e.clientY - startY;
-    banner.style.left = `${dx + initialLeft}px`;
-    banner.style.top = `${dy + initialTop}px`;
+    elem.style.left = `${dx + initialLeft}px`;
+    elem.style.top = `${dy + initialTop}px`;
   };
 
-  header.addEventListener('mousedown', (e) => {
+  // For the main banner
+  headerBanner.addEventListener('mousedown', (e) => {
     mouseDown = true;
     startX = e.clientX;
     startY = e.clientY;
     initialLeft = banner.offsetLeft;
     initialTop = banner.offsetTop;
-    window.addEventListener('mousemove', calculatePosition);
+    currentDragFunction = calculatePosition(banner); // Store reference to function
+    window.addEventListener('mousemove', currentDragFunction);
+  });
+
+  // For the popup
+  headerPopup.addEventListener('mousedown', (e) => {
+    mouseDown = true;
+    startX = e.clientX;
+    startY = e.clientY;
+    initialLeft = popup.offsetLeft;
+    initialTop = popup.offsetTop;
+    currentDragFunction = calculatePosition(popup); // Store reference to function
+    window.addEventListener('mousemove', currentDragFunction);
   });
 
   window.addEventListener('mouseup', () => {
     mouseDown = false;
-    window.removeEventListener('mousemove', calculatePosition);
+    if (currentDragFunction) {
+        window.removeEventListener('mousemove', currentDragFunction); // Remove the current dragging function
+        currentDragFunction = null; // Reset the function reference
+    }
   });
 
-  closeButton.addEventListener('click', () => {
-    document.querySelector(".container").style.display = "none";
-  })
+  showPopUp.addEventListener('click', () => {
+    if (!isPopupVisible) {
+      event.stopPropagation();
+      popupContainer.style.display = "block";
+      popup.style.display = "block";
+    } else {
+      popupContainer.style.display = "none";
+    }
+    isPopupVisible = !isPopupVisible; // Toggle the state
+  });
+
+  popupContainer.addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
+  
+  closeBannerButton.addEventListener('click', () => {
+    document.querySelector(".banner").style.display = "none";
+  });
+
+  closePopupButton.addEventListener('click', () => {
+    document.querySelector(".banner-pop").style.display = "none";
+  });
 };
