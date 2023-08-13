a027d88ce0fa1c57441a12e8e2452ae8a17da857
gianluca cattin <gcattin97@gmail.com>
2023-08-13 02:00:53 +0200

    a lot of new tings

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 window.onload = function() {
   const banner = document.querySelector('.banner');
-  const popup = document.querySelector('.banner-pop');
   const headerBanner = document.querySelector('.header');
+
+  const popup = document.querySelector('.banner-pop');
   const headerPopup = document.querySelector('.header-pop');
   const closeBannerButton = document.querySelector('#closeBannerBtn');
   const closePopupButton = document.querySelector('#closePopupBtn');
   const showPopUp = document.querySelector('#projects-folder')
   const popupContainer = document.querySelector('#popupContainer')
 
+  const bannerJpeg = document.querySelector('.banner-jpegs');
+  const headerJpeg = document.querySelector('.header-jpegs');
+  const jpegsFolder = document.querySelector('#jpegs-folder');
+  const jpegsContainer = document.querySelector('#jpegContainer');
+  const closeJpegsButton = document.querySelector('#closeJpegBtn');
+
+  const bannerRemilia = document.querySelector('.banner-nfts');
+  const headerRemilia = document.querySelector('.header-nfts');
+  const remiliaFolder = document.querySelector('#remilia-folder');
+  const remiliaNftsContainer = document.querySelector('#remiliaNftsContainer');
+  const closeRemiliaNftsButton = document.querySelector('#closeRemiliaNftsBtn');
+
+
+
   let mouseDown = false;
   let startX, startY, initialLeft, initialTop;
   let currentDragFunction = null;
   let isPopupVisible = false;
+  let isJpegsVisible = false;
+  let isRemiliaNftsVisible = false;
+
 
   const calculatePosition = (elem) => (e) => {
     const dx = e.clientX - startX;
@@ -64,12 +82,84 @@ window.onload = function() {
   popupContainer.addEventListener('click', (event) => {
     event.stopPropagation();
   });
-  
+
   closeBannerButton.addEventListener('click', () => {
     document.querySelector(".banner").style.display = "none";
   });
 
   closePopupButton.addEventListener('click', () => {
     document.querySelector(".banner-pop").style.display = "none";
+  });
+
+  // For jpegs folder
+
+  headerJpeg.addEventListener('mousedown', (e) => {
+    mouseDown = true;
+    startX = e.clientX;
+    startY = e.clientY;
+    initialLeft = bannerJpeg.offsetLeft;
+    initialTop = bannerJpeg.offsetTop;
+    currentDragFunction = calculatePosition(bannerJpeg); // Store reference to function
+    window.addEventListener('mousemove', currentDragFunction);
+  });
+
+  window.addEventListener('mouseup', () => {
+    mouseDown = false;
+    if (currentDragFunction) {
+        window.removeEventListener('mousemove', currentDragFunction); // Remove the current dragging function
+        currentDragFunction = null; // Reset the function reference
+    }
+  });
+
+  jpegsFolder.addEventListener('click', () => {
+    if (!isJpegsVisible) {
+      event.stopPropagation();
+      jpegsContainer.style.display = "block";
+    } else {
+      jpegsContainer.style.display = "none";
+    }
+    isJpegsVisible = !isJpegsVisible;
+  });
+
+
+  closeJpegsButton.addEventListener('click', () => {
+    jpegsContainer.style.display = "none";
+    isJpegsVisible = false;
+  });
+
+  // For Remilia popup
+
+  headerRemilia.addEventListener('mousedown', (e) => {
+    mouseDown = true;
+    startX = e.clientX;
+    startY = e.clientY;
+    initialLeft = bannerRemilia.offsetLeft;
+    initialTop = bannerRemilia.offsetTop;
+    currentDragFunction = calculatePosition(bannerRemilia); // Store reference to function
+    window.addEventListener('mousemove', currentDragFunction);
+  });
+
+  window.addEventListener('mouseup', () => {
+    mouseDown = false;
+    if (currentDragFunction) {
+        window.removeEventListener('mousemove', currentDragFunction); // Remove the current dragging function
+        currentDragFunction = null; // Reset the function reference
+    }
+  });
+
+  remiliaFolder.addEventListener('click', () => {
+    if (!isRemiliaNftsVisible) {
+      event.stopPropagation();
+      remiliaNftsContainer.style.display = "block";
+    } else {
+      remiliaNftsContainer.style.display = "none";
+    }
+    isRemiliaNftsVisible = !isRemiliaNftsVisible; // Toggle the state
+  });
+
+
+  closeRemiliaNftsButton.addEventListener('click', () => {
+    remiliaNftsContainer.style.display = "none";
+    isRemiliaNftsVisible = false;
   });
 };
